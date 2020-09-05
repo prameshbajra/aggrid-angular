c67893a3fb060b0dac5d8aaf5edc9248dc5b0eec
prameshbajra <pe.messh@gmail.com>
2020-09-05 19:52:48 +0530

    Ready for multi header now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'aggrid-table';
+    title = 'app';
+
+    columnDefs = [
+        { headerName: 'Make', field: 'make', sortable: true, filter: true },
+        { headerName: 'Model', field: 'model', sortable: true, filter: true },
+        { headerName: 'Price', field: 'price', sortable: true, filter: true }
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+
+    constructor() {
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AgGridModule.withComponents([])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
